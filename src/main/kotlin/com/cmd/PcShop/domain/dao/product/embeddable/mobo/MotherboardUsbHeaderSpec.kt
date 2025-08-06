67eb2fe874072dea646f629c610f0f2a19f305fc
Cmd_ <uchzhezherov@gmail.com>
2025-08-07 00:26:41 +0300

    MOBO related specs refactoring

--- src/main/kotlin/com/cmd/PcShop/domain/dao/product/embeddable/mobo/MotherboardUsbHeaderSpec.kt
+++ src/main/kotlin/com/cmd/PcShop/domain/dao/product/embeddable/mobo/MotherboardUsbHeaderSpec.kt
@@ -1,4 +1,4 @@
-package com.cmd.PcShop.domain.dao.product.embeddable
+package com.cmd.PcShop.domain.dao.product.embeddable.mobo
 
 import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
