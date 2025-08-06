c72a171ae4f603701bfd459db7508d04e9aead44
Cmd_ <uchzhezherov@gmail.com>
2025-08-06 21:57:42 +0300

    Removed nullable properties

--- src/main/kotlin/com/cmd/PcShop/domain/dao/product/embeddable/MotherboardPcieSpec.kt
+++ src/main/kotlin/com/cmd/PcShop/domain/dao/product/embeddable/MotherboardPcieSpec.kt
@@ -8,14 +8,14 @@ import jakarta.persistence.Embeddable
 class MotherboardPcieSpec (
     @Column(name = "pcie_version", nullable = false)
     var pcieVersion: PCIEVersion,
-    @Column(name = "pcie_x16_number")
-    var pcieX16Number: Int?,
-    @Column(name = "pcie_x8_number")
-    var pcieX8Number: Int?,
-    @Column(name = "pcie_x4_number")
-    var pcieX4Number: Int?,
-    @Column(name = "pcie_x1_number")
-    var pcieX1Number: Int?,
+    @Column(name = "pcie_x16_number", nullable = false)
+    var pcieX16Number: Int,
+    @Column(name = "pcie_x8_number", nullable = false)
+    var pcieX8Number: Int,
+    @Column(name = "pcie_x4_number", nullable = false)
+    var pcieX4Number: Int,
+    @Column(name = "pcie_x1_number", nullable = false)
+    var pcieX1Number: Int,
     @Column(name = "pcie_additional_info", length = 512)
     var pcieAdditionalInfo: String?,
 )
