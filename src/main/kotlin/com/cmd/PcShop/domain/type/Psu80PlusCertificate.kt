3be6d1f7b2a9a1d8fd9072babfa5e2dba6f6db7a
Cmd_ <uchzhezherov@gmail.com>
2025-08-10 23:50:22 +0300

    Psu domain enum

---
+++ src/main/kotlin/com/cmd/PcShop/domain/type/Psu80PlusCertificate.kt
@@ -0,0 +1,12 @@
+package com.cmd.PcShop.domain.type
+
+enum class Psu80PlusCertificate (
+    val value: String
+) {
+    BASIC("базовый"),
+    BRONZE("бронзовый"),
+    SILVER("серебряный"),
+    GOLD("золотой"),
+    PLATINUM("платиновый"),
+    TITANIUM("титановый")
+}
---
+++ src/main/kotlin/com/cmd/PcShop/domain/type/PsuFormFactor.kt
@@ -0,0 +1,11 @@
+package com.cmd.PcShop.domain.type
+
+enum class PsuFormFactor (
+    val value: String
+) {
+    ATX("ATX"),
+    FLEX_ATX("Flex ATX"),
+    SFX_L("SFX-L"),
+    SFX("SFX"),
+    TFX("TFX")
+}
