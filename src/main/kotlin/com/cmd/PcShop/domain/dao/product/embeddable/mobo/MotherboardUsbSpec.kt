b71af77aea9a5d53b2fb89bb15e7a4af9d18c529
Cmd_ <uchzhezherov@gmail.com>
2025-08-20 23:26:50 +0300

    Moved usb connectors that are the same for both cases and mobos into embeddable

--- src/main/kotlin/com/cmd/PcShop/domain/dao/product/embeddable/mobo/MotherboardUsbSpec.kt
+++ src/main/kotlin/com/cmd/PcShop/domain/dao/product/embeddable/mobo/MotherboardUsbSpec.kt
@@ -1,24 +1,14 @@
 package com.cmd.PcShop.domain.dao.product.embeddable.mobo
 
+import com.cmd.PcShop.domain.dao.product.embeddable.GeneralUsbConnectorsSpec
 import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
+import jakarta.persistence.Embedded
 
 @Embeddable
 class MotherboardUsbSpec (
-    @Column(name = "usb_2_number", nullable = false)
-    var usb2Number: Int,
-    @Column(name = "usb_32_gen_1_number", nullable = false)
-    var usb32Gen1Number: Int,
-    @Column(name = "usb_32_gen_2_number", nullable = false)
-    var usb32Gen2Number: Int,
-    @Column(name = "usb_32_gen_1_type_c_number", nullable = false)
-    var usb32Gen1TypeCNumber: Int,
-    @Column(name = "usb_32_gen_2_type_c_number", nullable = false)
-    var usb32Gen2TypeCNumber: Int,
-    @Column(name = "usb_32_gen_2x2_number", nullable = false)
-    var usb32Gen2x2Number: Int,
-    @Column(name = "usb_4_number", nullable = false)
-    var usb4Number: Int,
+    @Embedded
+    var generalUsbSpec: GeneralUsbConnectorsSpec,
     @Column(name = "usb_thunderbolt_3_number", nullable = false)
     var usbThunderbolt3Number: Int,
     @Column(name = "usb_thunderbolt_4_number", nullable = false)
