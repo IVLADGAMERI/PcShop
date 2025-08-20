95c563d35873fd704a51a37c3c891261d4a041a8
Cmd_ <uchzhezherov@gmail.com>
2025-08-20 23:24:40 +0300

    Case related enums

---
+++ src/main/kotlin/com/cmd/PcShop/domain/type/CaseFormFactor.kt
@@ -0,0 +1,11 @@
+package com.cmd.PcShop.domain.type
+
+enum class CaseFormFactor (
+    val value: String
+) {
+    SFF("SFF"),
+    MINI_TOWER("Mini Tower"),
+    MID_TOWER("Mid Tower"),
+    FULL_TOWER("Full Tower"),
+    SUPER_TOWER("Super Tower")
+}
---
+++ src/main/kotlin/com/cmd/PcShop/domain/type/CasePsuMountType.kt
@@ -0,0 +1,10 @@
+package com.cmd.PcShop.domain.type
+
+enum class CasePsuMountType (
+    val value: String
+) {
+    BOTTOM("снизу"),
+    TOP("сверху"),
+    FRONT("спереди"),
+    SIDE("сбоку")
+}
